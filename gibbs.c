5350ed42b90f5e7e727b2da2ce2eb5a10abcd920
Ryosuke Eto <ryo-s.1212@keio.jp>
2025-05-22 09:26:18 +0000

    手順2スコア行列の計算まで

--- gibbs.c
+++ gibbs.c
@@ -8,8 +8,10 @@
 #define MOTIF_LENGTH 7 //モチーフ長
 #define CHARACTER_NUM 4 //文字の種類数
 
+char character_type[CHARACTER_NUM] = {'A', 'C', 'G', 'T'}; // 塩基をchar型からint型に変換する配列
 double g_base_fre_table[CHARACTER_NUM] = {7519429.0, 4637676.0, 4637676.0, 7519429.0}; //塩基出現頻度
 double g_q[CHARACTER_NUM]={0.0}; //バックグラウンドの出現確率
+double g_score[CHARACTER_NUM][MOTIF_LENGTH]={0.0}; //スコア行列
 struct promoter{
   char name[BUFSIZE];
   char seq[BUFSIZE];
@@ -51,6 +53,15 @@ int cal_matrix_length(char matrix[]){
   return length;
 }
 
+//文字の種類の判定
+int check_char(char x){
+  int type = 0;
+  for(type = 0; type < CHARACTER_NUM; type++){
+    if(x == character_type[type]){break;}
+  }
+  return type;
+}
+
 //出現位置の初期化
 void init_pos(int matrix_n){
   srand((unsigned)time(NULL)); //乱数の初期化
@@ -76,13 +87,53 @@ void cal_back_q(void){
 }
 
 //モチーフ位置からのスコア行列の計算
-void cal_motif_score(int selected_num, int gene_i){
+void cal_motif_score(int selected_num, int matrix_n){
 
+  //スコア行列の初期化
+  for(int i = 0; i < CHARACTER_NUM; i++){
+    for(int j = 0; j < MOTIF_LENGTH; j++){
+      g_score[i][j] = 0;
+    }
+  }
+
+  //出現頻度の計算
+  for(int gene_i = 0; gene_i < matrix_n; gene_i++){
+    if(gene_i == selected_num){continue;} //選ばれた1本の場合計算しない
+
+    int start = g_pro[gene_i].pos; //モチーフの位置
+    for(int i = start; i < g_pro[gene_i].pos + MOTIF_LENGTH; i++){
+        g_score[check_char(g_pro[gene_i].seq[i])][i - start]++; //出現回数を加算  
+    }
+  }
+  
+  //スコアの表示
+  for(int i = 0; i < CHARACTER_NUM; i++){
+    printf("\n");
+    for(int j = 0; j < MOTIF_LENGTH; j++){
+      printf("%5.0f ", g_score[i][j]);
+    }
+  }
+
+  //スコア行列の計算
+  for(int i = 0; i < CHARACTER_NUM; i++){
+    printf("\n");
+    for(int j = 0; j < MOTIF_LENGTH; j++){
+      g_score[i][j]++; //疑似頻度1を加算
+      g_score[i][j] /= (matrix_n - 1); //出現確率の計算
+      g_score[i][j] /= g_q[i]; //スコア行列の計算
+      //printf("%1.5f ", g_score[i][j]);
+    }
+  }
 }
 
 //ギブスサンプリングによる結合部位の発見
 int gibbs_scan(int matrix_n){
   init_pos(matrix_n);
+
+  for(int gene_i = 0; gene_i < matrix_n; gene_i++){ //プロモータの中から一本選ぶ
+    cal_motif_score(gene_i, matrix_n); //選んだ1本以外の配列からスコア行列を作成
+
+  }
 
 
   
@@ -90,13 +141,9 @@ int gibbs_scan(int matrix_n){
 
 int main(int argc, char* argv[]){
   int gene_num = read_promoter(argv[1]);  //1番目の引数で指定した遺伝子のプロモータ領域を読み込む
-
+  cal_back_q();
   gibbs_scan(gene_num);
 
-cal_back_q();
 
-  for(int i=0; i<gene_num; i++){
-    printf("%d\n", g_pro[i].pos);
-  }
   return 0;
 }
